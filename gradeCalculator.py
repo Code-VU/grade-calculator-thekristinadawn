671040e4b5bd7f2d4a42aca74bb01dc3cae72ac5
Kristina Palmer <Kristina.Palmer@vu.com>
2022-02-01 15:15:47 -0600

    First Attempt

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,7 +3,23 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+    score = float(input("Enter score:"))
+    float_score=score
+    try: 
+        if float_score < 0.0 or float_score> 1.0: 
+            print("Bad score")
+        elif float_score >= 0.9:
+            print("A")
+        elif float_score >= 0.8:
+            print("B")
+        elif float_score >= 0.7:
+            print("C")
+        elif float_score >= 0.6:
+            print("D")
+        elif float_score < 0.6:
+            print("F")
+    except:
+        print("Please enter a number 0.0 - 1.0")    
 
     # end assignment
 
